04c3bd961c2021d6a9b6569b4c0d8d8862fda61f
Matthew Cline <cline.48@wright.edu>
2017-10-24 06:56:10 -0400

    Finished Project, still need to clean up and generalize code.

--- main.cpp
+++ main.cpp
@@ -14,26 +14,57 @@ public:
 	Mat clean_edges;
 	Mat smoothed;
 	Mat lines;
+	Mat gradDirect;
+	Mat gradMag;
 };
 
 std::pair<double, double> calc_gradient(Mat image, int row, int col)
 {
-	int tempX = image.at<int>(row+1, col) - image.at<int>(row - 1, col);
-	int tempY = image.at<int>(row, col+1) - image.at<int>(row, col - 1);
+	float tempX = image.at<int>(row+1, col) - image.at<int>(row - 1, col);
+	float tempY = image.at<int>(row, col+1) - image.at<int>(row, col - 1);
 
 	double mag = sqrt(pow(tempX, 2) + pow(tempY, 2));
-	double direction = atan2(tempY, tempX);
-
-	if(direction < 0)
-	{
-		direction += 2*M_PI;
-	}
+	double direction = atan(tempY/tempX);
 
 	return std::pair<double, double>(mag, direction);
 }
 
+
+std::vector<int> findThetaPeaks(std::vector< std::pair<int, std::vector<int> > > hist)
+{
+	std::vector<int> peaks;
+
+	for(int i = 1; i < hist.size() - 1; ++i)
+	{
+		if(hist[i].first > hist[i-1].first && hist[i].first > hist[i+1].first)
+		{
+			peaks.push_back(i);
+		}
+	}
+
+	return peaks;
+}
+
+std::vector<int> findRhoPeaks(std::vector<int> hist)
+{
+	std::vector<int> peaks;
+
+	for(int i = 1; i < hist.size() - 1; ++i)
+	{
+		if(hist[i] > hist[i-1] && hist[i] > hist[i+1])
+		{
+			peaks.push_back(i);
+		}
+	}
+
+	return peaks;
+}
+
+
+
 int main(int argc, char** argv){
 
+	/////// READ IN THE IMAGES TO CUSTOM CLASS ///////
 	ImObj box;
 	ImObj pentagon;
 
@@ -51,9 +82,26 @@ int main(int argc, char** argv){
 	pentagon.edges = imread(fn + "pentagonCanny2.pgm", 0);
 	pentagon.clean_edges = imread(fn + "pentagonCannyClean", 0);
 	pentagon.smoothed = imread(fn + "pentagonSmooth2.pgm", 0);
+	pentagon.gradDirect = pentagon.src;
+	pentagon.gradMag = pentagon.src;
 	cvtColor(pentagon.src, pentagon.lines, CV_GRAY2BGR);
 
 
+	/////// GLOBAL VARIABLE INTITIALIZATIONS ///////
+	int edgeThresh = 45;
+	int thetaBins = 35;
+	int rhoBins = 10;
+	float thetaBinSize = M_PI / thetaBins;
+	float rhoMax = sqrt(pow(pentagon.src.rows, 2) + pow(pentagon.src.cols, 2));
+	float rhoBinSize = rhoMax / rhoBins;
+	std::vector<std::pair<int, std::vector<int> > > houghTrans(thetaBins);
+	for (int i = 0; i < houghTrans.size(); ++i)
+	{
+		houghTrans[i] = std::pair<int, std::vector<int> >(0, std::vector<int>(rhoBins));
+	}
+
+	/////// BUILD THE THETA HISTOGRAM FOR THE PENTAGON IMAGE ///////
+	vector<vector<pair<int, int> > > gradDirHist(thetaBins);
 	for(int i = 0; i < pentagon.edges.rows; ++i)
 	{
 		for(int j = 0; j < pentagon.edges.cols; ++j)
@@ -61,29 +109,97 @@ int main(int argc, char** argv){
 			if(pentagon.edges.at<int>(i, j))
 			{
 				std::pair<double, double> grad = calc_gradient(pentagon.smoothed, i, j);
-//				cout << "Gradient Mag: " << grad.first << endl;
-//				cout << "Gradient Direction: " << grad.second << endl << endl;
+
+				for(int k = 0; k < thetaBins; ++k)
+				{
+					float thetaCalc = (-(M_PI / 2)) + k * thetaBinSize;
+					float rhoCalc = i*cos(thetaCalc) + j*sin(thetaCalc);
+					if(k == 0)
+					{
+						if(grad.second <= (-(M_PI / 2) + thetaBinSize))
+						{
+							houghTrans[k].first += 1;
+
+							for(int h = 0; h < rhoBins; ++h)
+							{
+								if(h == 0)
+								{
+									if(rhoCalc <= rhoBinSize)
+									{
+										houghTrans[k].second[h] += 1;
+										break;
+									}
+								}
+
+								if(rhoCalc > rhoBinSize * h && rhoCalc <= rhoBinSize * (h+1))
+								{
+									houghTrans[k].second[h] += 1;
+									break;
+								}
+							}
+							break;
+						}
+					}
+
+					if(grad.second > (-(M_PI / 2) + (thetaBinSize * k)) && grad.second <= (-(M_PI / 2) + (thetaBinSize * (k + 1))))
+					{
+						houghTrans[k].first += 1;
+						for(int h = 0; h < rhoBins; ++h)
+						{
+							if(h == 0)
+							{
+								if(rhoCalc <= rhoBinSize)
+								{
+									houghTrans[k].second[h] += 1;
+									break;
+								}
+							}
+
+							if(rhoCalc > rhoBinSize * h && rhoCalc <= rhoBinSize * (h+1))
+							{
+								houghTrans[k].second[h] += 1;
+								break;
+							}
+						}
+						break;
+					}
+				}
 			}
 		}
 	}
 
-	vector<Vec2f> lines;
-	HoughLines(pentagon.src, lines, 1, CV_PI/180, 150, 0, 0);
 
-	for(size_t i = 0; i < lines.size(); ++i)
+
+	/////// EVALUATE THE HISTOGRAMS OF THETA AND RHO ///////
+	std::vector<int> thetaPeaks = findThetaPeaks(houghTrans);
+
+	for(int i = 0; i < thetaPeaks.size(); ++i)
 	{
-		float rho = lines[i][0], theta = lines[i][i];
-		Point pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a*rho, y0 = b*rho;
-		pt1.x = cvRound(x0 + 1000*(-b));
-		pt1.y = cvRound(y0 + 1000*(a));
-		pt2.x = cvRound(x0 - 1000*(-b));
-		pt2.y = cvRound(y0 - 1000*(a));
-		line(pentagon.lines, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+		float theta = (-(M_PI / 2)) + thetaBinSize * thetaPeaks[i];
+		std::vector<int> rhoPeaks = findRhoPeaks(houghTrans[thetaPeaks[i]].second);
+
+		for(int j = 0; j < rhoPeaks.size(); ++j)
+		{
+			if(houghTrans[thetaPeaks[i]].second[rhoPeaks[j]] < edgeThresh)
+			{
+				continue;
+			}
+
+			float rho = rhoBinSize * rhoPeaks[j];
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a*rho, y0 = b*rho;
+			pt1.x = cvRound(x0 + 1000*(-b));
+			pt1.y = cvRound(y0 + 1000*(a));
+			pt2.x = cvRound(x0 - 1000*(-b));
+			pt2.y = cvRound(y0 - 1000*(a));
+			line(pentagon.lines, pt1, pt2, Scalar(0,0,255), 1, CV_AA);
+		}
 	}
 
-	imshow("Detected Lines", pentagon.lines);
+
+	/////// DISPLAY THE IMAGE WITH THE LINES DRAWN ////////
+	imshow("Pentagon Lines", pentagon.lines);
 	waitKey();
 
 	return 0;
